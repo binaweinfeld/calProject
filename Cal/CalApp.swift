c07acad82968fc4c49680858c22615a50a36e722
Michal <michalp752@gmail.com>
2024-10-08 08:51:42 +0300

    Initial Commit

---
+++ Cal/CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalApp.swift
+//  Cal
+//
+//  Created by michal zak on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
